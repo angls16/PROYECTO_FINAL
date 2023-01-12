03f3199abd9a5beff7fbff940e33e30c84afb2ff
Angel Caiza <angel.dvd.02@gmail.com>
2023-01-12 08:08:43 -0500

    Agregar archivos de proyecto.

---
+++ PROYECTO_FINAL/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PROYECTO_FINAL
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new menu();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PROYECTO_FINAL/generar_clave.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PROYECTO_FINAL
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class generar_clave : ContentPage
+    {
+        public generar_clave()
+        {
+            InitializeComponent();
+        }
+    }
+}
