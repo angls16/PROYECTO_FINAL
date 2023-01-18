1e3e3bf1db7757ae24d46cb9b965443194ce6070
Angel Caiza <angel.dvd.02@gmail.com>
2023-01-18 09:55:07 -0500

    cambios

--- PROYECTO_FINAL/generar_clave.xaml.cs
+++ PROYECTO_FINAL/generar_clave.xaml.cs
@@ -16,5 +16,37 @@ namespace PROYECTO_FINAL
         {
             InitializeComponent();
         }
+        string Permutacion = "";
+        public void bloque(object sender, EventArgs e) {
+            Button boton = (Button)sender;
+            if(boton.Text == "8"){
+                Permutacion = "1234" ;
+
+            }
+            else if (boton.Text == "10")
+            {
+                Permutacion = "12345";
+            }
+            else if (boton.Text == "12")
+            {
+                Permutacion= "123456";
+            }
+            permutacion.Text = Permutacion;
+        }
+        private void limpiar_Clicked(object sender, EventArgs e)
+        {
+            permutacion.Text = String.Empty;
+            sustitucion.Text = String.Empty;
+            interaciones.Text = String.Empty;
+            generado.Text = String.Empty;
+
+        }
+        private void generar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void copiar_Clicked(object sender, EventArgs e)
+        {
+        }
     }
 }
